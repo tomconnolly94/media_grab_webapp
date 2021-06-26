c97ea5183ababda72e960e47763e675f240fe764
tom <tom.connolly@protonmail.com>
2021-06-26 14:06:18 +0200

    ADDED: coloured rows to indicated tv shows currently being released

--- client/js/index.js
+++ client/js/index.js
@@ -5,6 +5,10 @@ function loadMediaIndexJson(){
 
     var mediaIndexList = response.data["media"];
 
+    // for(var i = 0; i < mediaIndexList.length; i++){
+    //   mediaIndexList[i]["showBeingReleased"] = mediaIndexList["typeSpecificData"]["latestEpisode"] != 1;
+    // }
+
     new Vue({
       el : '#mediaIndexContent',
       data () { 
