f8a1cb51672bfc3fa6a87389afed590eff4d5efd
tom <tom.connolly@protonmail.com>
2021-06-28 10:57:57 +0200

    ADDED: show details can be edited on click

--- client/js/index.js
+++ client/js/index.js
@@ -1,30 +1,70 @@
+Vue.directive('clickaway', {
+	bind(el, { value }) {
+		if (typeof value !== 'function') {
+			console.warn(`Expect a function, got ${value}`)
+			return
+		}
+
+		document.addEventListener('click', e => el.contains(e.target) || value())
+	}
+});
+
+function loadMediaIndexJson() {
+	axios.get(`/MediaIndex`).then((response) => {
+
+		var mediaInfoList = response.data["media"];
+
+		mediaInfoList = reformatMediaIndexData(mediaInfoList);
+
+		new Vue({
+			el: '#mediaIndexContent',
+			data() {
+				return {
+					content: mediaInfoList
+				}
+			},
+			methods: {
+				xButtonClicked: function (recordName) {
+					axios.delete(`/MediaInfoRecord?recordName=${recordName}`).then((response) => {
+						window.location.href = "/";
+					});
+				},
+				inputConfirmed: function(item, changedFieldName){
+					console.log(item);
+					console.log(changedFieldName);
+				}
+			}
+		})
+	});
+}
 
 
-function loadMediaIndexJson(){
-  axios.get(`/MediaIndex`).then((response) => {
+function addEditFieldToObject(obj, relevantKey){
 
-    var mediaIndexList = response.data["media"];
+	var newObj = {
+		edit: false
+	}
+	newObj["content"] = obj[relevantKey];
 
-    // for(var i = 0; i < mediaIndexList.length; i++){
-    //   mediaIndexList[i]["showBeingReleased"] = mediaIndexList["typeSpecificData"]["latestEpisode"] != 1;
-    // }
+	obj[relevantKey] = newObj;
+}
 
-    new Vue({
-      el : '#mediaIndexContent',
-      data () { 
-        return {
-          content: mediaIndexList
-        } 
-      },
-      methods: {
-        xButtonClicked: function(recordName) {
-          axios.delete(`/MediaInfoRecord?recordName=${recordName}`).then((response) => {
-            window.location.href = "/";
-          });
-        }
-      }
-    }) 
-  });
+
+function reformatMediaIndexData(mediaIndexData) {
+	mediaIndexData.forEach(item => {
+		Object.keys(item).forEach(key => {
+			if(Object.prototype.toString.call(item[key]) === '[object Object]'){
+				var subItem = item[key];
+				Object.keys(subItem).forEach(subKey => {
+					addEditFieldToObject(subItem, subKey);
+				});				
+			}
+			else{
+				addEditFieldToObject(item, key);
+			}			
+		});
+	});
+	return mediaIndexData;
 }
 
 loadMediaIndexJson();
